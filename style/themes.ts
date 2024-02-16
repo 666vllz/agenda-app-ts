c81e3daf0fbe8ee7c0f79c90ca23703da60e7f87
Luis Torres <luis.i6nacio@gmail.com>
2024-02-16 02:38:37 -0300

    add unistyles basic config

---
+++ style/themes.ts
@@ -0,0 +1,35 @@
+export const lightTheme = {
+  colors: {
+    primary: "#3e63dd",
+    textPrimary: "#0a0a0a",
+    textSecondary: "#d6d6d6",
+    textTertiary: "#868686",
+    backgroundPrimary: "#ffffff",
+    backgroundSecondary: "#f6f6f6",
+    border: "#0000001f",
+  },
+  margins: {
+    sm: 2,
+    md: 4,
+    lg: 8,
+    xl: 12,
+  },
+} as const
+
+export const darkTheme = {
+  colors: {
+    primary: "#3e63dd",
+    textPrimary: "#f6f6f6",
+    textSecondary: "#d6d6d6",
+    textTertiary: "#868686",
+    backgroundPrimary: "#161616",
+    backgroundSecondary: "#1f1f1f",
+    border: "#ffffff1f",
+  },
+  margins: {
+    sm: 2,
+    md: 4,
+    lg: 8,
+    xl: 12,
+  },
+} as const
